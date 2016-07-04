1cb00a5d37b05560d14bb740a57c6eb712241f2d
Juho Jaakkola <juho@elgg.org>
2016-07-04 12:42:48 +0300

    The site logo is now a link to front page
    Fixes #3

--- views/default/page/elements/topbar_wrapper.php
+++ views/default/page/elements/topbar_wrapper.php
@@ -9,9 +9,14 @@
 <div class="elgg-page-topbar">
 	<div class="elgg-inner">
 		<?php
-			echo elgg_view('output/img', array(
+			$img = elgg_view('output/img', array(
 				'src' => elgg_get_simplecache_url('graphics/logo.png'),
 				'alt' => 'Social business world'
+			));
+
+			echo elgg_view('output/url', array(
+				'href' => elgg_is_logged_in() ? '/activity' : '/',
+				'text' => $img,
 			));
 
 			echo elgg_view('page/elements/topbar', $vars);
