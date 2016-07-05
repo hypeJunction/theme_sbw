28b92425c70ee0448a9af5027be13c2ecdd754ef
Juho Jaakkola <juho@elgg.org>
2016-07-05 20:14:38 +0300

    Add language selector to topbar
    Fixes #1

--- start.php
+++ start.php
@@ -12,6 +12,8 @@ function theme_sbw_init() {
 	elgg_extend_view('elgg.css', 'theme_sbw.css');
 
 	elgg_unextend_view('page/elements/sidebar', 'search/header');
+
+	elgg_extend_view('page/elements/topbar', 'language_selector/default');
 }
 
 /**
