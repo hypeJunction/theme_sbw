30c3457793fa5bd84c76057199be29a6958eb610
Juho Jaakkola <juho@elgg.org>
2016-07-05 20:07:04 +0300

    Use margin instead of text-align to center the site logo
    This makes it be located in the center regardless of which
    other elements are added to the topbar.

--- views/default/page/elements/topbar_wrapper.php
+++ views/default/page/elements/topbar_wrapper.php
@@ -9,6 +9,8 @@
 <div class="elgg-page-topbar">
 	<div class="elgg-inner">
 		<?php
+			echo elgg_view('page/elements/topbar', $vars);
+
 			$img = elgg_view('output/img', array(
 				'src' => elgg_get_simplecache_url('graphics/logo.png'),
 				'alt' => 'Social business world'
@@ -17,10 +19,9 @@
 			echo elgg_view('output/url', array(
 				'href' => elgg_is_logged_in() ? '/activity' : '/',
 				'text' => $img,
+				'id' => 'site-logo',
 			));
-
-			echo elgg_view('page/elements/topbar', $vars);
 		?>
 
 	</div>
-</div>
+</div>,
