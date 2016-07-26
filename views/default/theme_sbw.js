57a8797d723ebededb6946bcd85dd56aebd5e507
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-07-26 12:34:39 +0200

    Restyle lightbox

--- views/default/theme_sbw.js
+++ views/default/theme_sbw.js
@@ -1,12 +1,27 @@
 /**
- * Site menu toggle
+ * Site wide bindings
  */
-define(function(require) {
-	$ = require('jquery');
+define(function (require) {
+	var elgg = require('elgg');
+	var $ = require('jquery');
 
-	$(document).on('click', '.sbw-off-canvas-toggle', function(e) {
+	$(document).on('click', '.sbw-off-canvas-toggle', function (e) {
 		e.preventDefault();
 		$('.elgg-page').toggleClass('elgg-page-show-off-canvas');
 		$('.sbw-off-canvas-toggle').toggleClass('elgg-state-active');
+	});
+
+	function getLightboxOptions(hook, type, params, options) {
+		return $.extend({}, options, {
+			current: elgg.echo('js:lightbox:current', ['{current}', '{total}']),
+			previous: '<span class="fa fa-caret-left"></span>',
+			next: '<span class="fa fa-caret-right"></span>',
+			close: '<span class="fa fa-times"></span>',
+			opacity: 0.5
+		});
+	}
+
+	require(['elgg/ready'], function () {
+		elgg.register_hook_handler('getOptions', 'ui.lightbox', getLightboxOptions);
 	});
 });
